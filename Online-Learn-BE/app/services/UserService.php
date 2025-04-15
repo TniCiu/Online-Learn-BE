d718d331e0dd347018fdf8d38df1b9adb90d8283
Tanhoccode <tanprovip156@gmail.com>
2025-04-15 14:52:51 +0700

    Init successfull crud user

---
+++ Online-Learn-BE/app/services/UserService.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    protected $repo;
+
+    public function __construct(UserRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function getAll() { return $this->repo->all(); }
+    public function find($id) { return $this->repo->find($id); }
+
+    public function create(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        $data['id'] = Crypt::encryptString($id); 
+        return $this->repo->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $user = $this->repo->find($id);
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return $this->repo->update($user, $data);
+    }
+
+    public function delete($id)
+    {
+        $user = $this->repo->find($id);
+        return $this->repo->delete($user);
+    }
+}
